70f7ac474730937bedc9bc884feeeaa6b35ac261
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-01-25 17:01:23 -0500

    fun with var

---
+++ app.js
@@ -0,0 +1,30 @@
+var funVar1 = 10
+var funVar2 = 20
+var funVar3 = 30
+var funVar4 = 40
+var funVar5 = 50
+var funVar6 = 60
+var funVar7 = 70
+var funVar8 = 80
+var funVar9 = 90
+var funVar10 = 100
+
+//console of addition below
+console.log(funVar3 + funVar6)
+console.log(funVar8 + funVar5)
+
+// console of subtraction below
+console.log(funVar6 - funVar4)
+console.log(funVar9 - funVar7)
+
+// console of multiplication below
+console.log(funVar2 * funVar1)
+console.log(funVar3 * funVar2)
+
+// console of division below
+console.log(funVar10 / funVar1)
+console.log(funVar8 / funVar4)
+
+//increment and decrement
+console.log(funVar1++)
+console.log(funVar10--)
